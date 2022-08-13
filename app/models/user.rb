bb62d852bb8d9364025ef9c347ebe42cff031090
Yajin13 <mfps.crazy@gmail.com>
2022-08-13 22:21:51 +0900

    passwordのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,9 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   validates :first_kana_name, presence: true
   validates :last_kana_name, presence: true
+  validates :birth_date, presence: true
+
+  VALID_PASSWORD_REGEX = (/[a-z\d]{6,}/i).freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります’}
 
 end
