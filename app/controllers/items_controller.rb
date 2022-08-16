0e77f98ad15f322b55b06acb60b9f2db58e27658
Yajin13 <mfps.crazy@gmail.com>
2022-08-16 21:54:01 +0900

    ご指摘事項修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :edit_show, only: [:edit, :show]
+  before_action :set_item, only: [:edit, :show, :update]
   before_action :ensure_user, only: [:edit, :update]
 
   def index
@@ -20,12 +20,14 @@ class ItemsController < ApplicationController
     end
   end
 
-  def edit_show
-    @item = Item.find(params[:id])
+  def show
   end
 
+  def edit
+  end
+
+
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -51,5 +53,10 @@ class ItemsController < ApplicationController
       redirect_to root_path
     end
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 
 end
